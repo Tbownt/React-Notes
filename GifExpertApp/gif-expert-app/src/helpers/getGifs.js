18c457c5b733dfa5c9da5b2b27932ba479c64444
Tbownt <98657140+Tbownt@users.noreply.github.com>
2023-03-08 16:45:09 -0400

    env agregado

--- GifExpertApp/gif-expert-app/src/helpers/getGifs.js
+++ GifExpertApp/gif-expert-app/src/helpers/getGifs.js
@@ -1,7 +1,7 @@
-const api_key = "U2JcARmEEoohvS73sS9SfTYBIIEitqet";
-
+const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
+// const api_key = "U2JcARmEEoohvS73sS9SfTYBIIEitqet";
 export const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=10`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`;
   const peticion = await fetch(url);
   const { data } = await peticion.json();
   const gifs = data.map((img) => ({
