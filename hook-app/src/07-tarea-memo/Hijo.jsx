e2beb1d3b9fdd2a9976a173e7aee3278ff63c01b
Tbownt <98657140+Tbownt@users.noreply.github.com>
2023-03-22 15:53:28 -0400

    rename of branch

--- hook-app/src/07-tarea-memo/Hijo.jsx
+++ hook-app/src/07-tarea-memo/Hijo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const Hijo = React.memo(({ numero, incrementar }) => {
-  console.log("  Me volví a generar :(  ");
+  console.log("  Me volví a generar :(...  ");
 
   return (
     <button
