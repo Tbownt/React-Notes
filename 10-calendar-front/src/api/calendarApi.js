1e6ccc7a73538a89a10913a7bf8f12e97d0adde8
Tbownt <98657140+Tbownt@users.noreply.github.com>
2023-05-16 18:28:42 -0400

    error en variables de entorno

--- 10-calendar-front/src/api/calendarApi.js
+++ 10-calendar-front/src/api/calendarApi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { getEnvVariables } from "../helpers/getEnvVariables";
+// import { getEnvVariables } from "../helpers/getEnvVariables";
 
-const { VITE_API_URL } = getEnvVariables();
+// const { VITE_API_URL } = getEnvVariables();
 
 const calendarApi = axios.create({
-  baseURL: VITE_API_URL,
+  baseURL: "https://tbowntcalendarback.up.railway.app/api",
 });
 
 calendarApi.interceptors.request.use((config) => {
